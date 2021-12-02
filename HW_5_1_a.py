1f14fd94d4d66e956ada4da54871d15a9ec3fa06
Soumyajit Saha <57801484+Soumyajit-Saha@users.noreply.github.com>
2021-12-01 20:49:55 -0700

    Update HW_5_1_a.py

--- HW_5_1_a.py
+++ HW_5_1_a.py
@@ -7,27 +7,31 @@ Created on Thu Nov 18 20:58:12 2021
 import random
 
 pop=[0]*10000000
-a=0.52
-b=0.48
+a=0.55
+b=0.45
 
 r=a*10000000
 for i in range(int(r)):
     pop[i]=1
 
 random.shuffle(pop)
-val=2500
+val=1000
 
+samples=[int(val*1.1**i) for i in range(100)]
 
-c=0
-for i in range(100):
-    sample_ind=random.sample(range(0,10000000),val)
-    sample=[]
-    for j in sample_ind:
-        sample.append(pop[j])
-    maj=sample.count(1)
-    if maj>0.5*val:
-        c+=1
-ans=c/100
-print(ans)
-
+for k in samples:
+    c=0
+    for i in range(100):
+        sample_ind=random.sample(range(0,10000000),k)
+        sample=[]
+        for j in sample_ind:
+            sample.append(pop[j])
+        maj=sample.count(1)
+        if maj>0.5*val:
+            c+=1
+    ans=c/100
+    # print(ans)
+    if ans>0.9:
+        print('YES', ans, k)
+        break
         
